f29034b5699ab9af84d72dfc85fac176c05d293a
Tinotsu <tinopionf@gmail.com>
2024-12-08 08:04:31 +0100

    product page

--- assets/card-product.js
+++ assets/card-product.js
@@ -1,26 +1,23 @@
-function initQuantitySelectors() {
-  const quantitySelectors = document.querySelectorAll(".quantity-selector");
-
-  quantitySelectors.forEach((selector) => {
-    const container = selector.closest(".form_button_cart");
-    const addToCartButton = container.querySelector(".add-to-cart-button");
-    const priceDisplay = container.querySelector(".price-display");
-
-    if (selector && addToCartButton && priceDisplay) {
-      const updateButtonText = () => {
-        const selectedOption = selector.options[selector.selectedIndex];
-        const selectedPrice = selectedOption?.getAttribute("data-price");
-        priceDisplay.textContent = selectedPrice ? ` ${selectedPrice}` : "";
-      };
-
-      updateButtonText();
-      selector.addEventListener("change", updateButtonText);
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    const quantitySelectors = document.querySelectorAll('.quantity-selector');
+  
+    quantitySelectors.forEach((selector) => {
+      const container = selector.closest('.form_button_cart'); 
+      const addToCartButton = container.querySelector('.add-to-cart-button');
+      const priceDisplay = container.querySelector('.price-display');
+  
+      if (selector && addToCartButton && priceDisplay) {
+        const updateButtonText = () => {
+          const selectedOption = selector.options[selector.selectedIndex];
+          const selectedPrice = selectedOption.getAttribute('data-price');
+          priceDisplay.textContent = selectedPrice ? ` ${selectedPrice}` : '';
+        };
+  
+        updateButtonText();
+  
+        selector.addEventListener('change', updateButtonText);
+      }
+    });
   });
-}
-
-// Initialisation lors du chargement de la page
-document.addEventListener("DOMContentLoaded", initQuantitySelectors);
-
-// Réinitialisation pour les sections dynamiques dans Shopify
-document.addEventListener("shopify:section:load", initQuantitySelectors);
+  
+  
